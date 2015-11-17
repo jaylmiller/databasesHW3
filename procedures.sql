258bc09423b4fe507f1271f92d54b68c5ec949e9
trevoraron <taron1@jhu.edu>
2015-11-17 00:10:07 -0500

    did update

--- procedures.sql
+++ procedures.sql
@@ -14,4 +14,18 @@ CREATE PROCEDURE AllRawScores(IN password VARCHAR(20))
     END IF;
 //
 
+/* For Part G */
+DROP PROCEDURE IF EXISTS UpdateMidterm //
+CREATE PROCEDURE UpdateMidterm(In password VARCHAR(20), In s_ssn INT, IN new INT)
+    IF NOT EXISTS (SELECT * from Passwords WHERE CurPasswords like password) THEN
+        SELECT 'Update Failed!' as 'Output';
+    ELSEIF NOT EXISTS (SELECT * from Rawscores WHERE SSN = s_ssn) THEN
+        SELECT 'Update Failed!' as 'Output';
+    ELSE
+        UPDATE Rawscores
+        Set Midterm = new
+        WHERE SSN = s_ssn;
+        SELECT 'Update Successful!' as 'Output';
+    END IF;
+//
 
